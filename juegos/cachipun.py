1edf5e44b056446907071a00f595d626c24c9d7b
nachotagle <163325892+nachotagle@users.noreply.github.com>
2024-03-15 19:22:40 +0000

    Agrege el CACHIPUN

--- juegos/cachipun.py
+++ juegos/cachipun.py
@@ -1,4 +1,28 @@
+import random
 def cachipun():
+    a = random.randint(1,3)
+    inpu = input('\nTira algo: ')
+    if a == 1:
+        resulta = 'Papel'
+    elif a == 2:
+        resulta = 'Piedra'
+    elif a == 3:
+        resulta = 'Tijera'
+    
+    print('\n' + resulta, 'vs', inpu)
+    gano = False
+    empate = False
+    if resulta == inpu:
+        empate = True
+        print('\nEMPATE!')
+    elif resulta == 'Tijera' and inpu == 'Piedra' or resulta == 'Papel' and inpu == 'Tijera' or resulta == 'Piedra' and inpu == 'Papel':
+        gano = True
+    
+    if gano:
+        print('\nGANASTE!!')
+    elif not gano and not empate:
+        print('\nPERDISTE!')
+    
     """
     Esta función representa el juego de cachipun.
     Debes pedir al usuario que elija piedra, papel o tijera, y luego comparar su elección con la de la computadora.
